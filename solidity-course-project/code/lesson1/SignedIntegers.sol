dac1073e0e0b09333d4c250d63eef08310eec981
Sewasew Tadele <138241812+sewasewT7@users.noreply.github.com>
2026-05-04 12:25:00 +0300

    Delete solidity-course-project/code/lesson1 directory

--- solidity-course-project/code/lesson1/SignedIntegers.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-    int8 public a = 20;     
-    int8 public b = -10;  
-
-    int16 public difference = a - b; // 20 - (-10) = 30
-}

18adbedda5f3e9d141dfae6abe52171126cc6aaf
Sewasew Tadele <138241812+sewasewT7@users.noreply.github.com>
2026-05-04 10:04:00 +0300

    Add Arguments.sol contract with increment and add functions

---
+++ solidity-course-project/code/lesson1/Arguments.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint256 public x;
+    
+    constructor(uint256 initialX) {
+        x = initialX;
+    }
+    
+    function increment() external {
+        x = x + 1;
+    }
+    function add(uint256 value) external view returns(uint256) {
+        return x + value;
+    }
+}

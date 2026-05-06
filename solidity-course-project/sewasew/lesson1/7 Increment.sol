95596a03e31594ab88d00d451d40f77d091ac0d8
Sewasew Tadele <138241812+sewasewT7@users.noreply.github.com>
2026-05-06 03:34:31 +0300

    Create Increment contract with increment and add functions

---
+++ solidity-course-project/sewasew/lesson1/7 Increment.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint256 public x;
+    
+    constructo(uint256 initialX) {
+        x = initialX;
+    }
+    function increment() external {
+        x = x + 1;
+    }
+     function add(uint256 value) external view returns(uint256) {
+        return x + value;
+    }
+}
+    
